05019d79832d4c4b72032dde7c04ed95b0b2094d
Dale Thatcher <github@dalethatcher.com>
2012-05-10 18:38:49 +0100

    Append posts from mongo to page.

--- src/ldncljweb/views/root.clj
+++ src/ldncljweb/views/root.clj
@@ -3,12 +3,17 @@
         [noir.response :only [content-type]]
         [hiccup.core :only [html]]
         [net.cgrand.enlive-html :only [deftemplate append html-snippet]])
+  (:require [ldncljweb.models.posts :as posts])
   (:import [java.util Date]))
 
 (deftemplate main "public/index.html" [message]
     [:#posts]
-             (append (html-snippet (html [:p message]))))
+             (append (html-snippet (html message))))
 
 (defpage "/" []
   (content-type "text/html"
-    (main (str "Hello templates! The time is " (Date.)))))
+    (main (html (map (fn [p] [:div {:class "post"}
+                              [:h1 (p :title)]
+                              [:p {class "date"} (p :date)]
+                              [:p {class "post-body"} (p :body)]])
+                     (posts/find-most-recent-five))))))
