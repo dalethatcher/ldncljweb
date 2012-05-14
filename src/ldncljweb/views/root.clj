f90e8f318d6cb246f0f1b514f420d18065bfa604
Dale Thatcher <github@dalethatcher.com>
2012-05-14 23:39:29 +0100

    Load content from db.

--- src/ldncljweb/views/root.clj
+++ src/ldncljweb/views/root.clj
@@ -2,18 +2,18 @@
   (:use [noir.core :only [defpage]]
         [noir.response :only [content-type]]
         [hiccup.core :only [html]]
-        [net.cgrand.enlive-html :only [deftemplate append html-snippet]])
+        [net.cgrand.enlive-html :only [deftemplate append html-snippet html-content]])
   (:require [ldncljweb.models.posts :as posts])
   (:import [java.util Date]))
 
 (deftemplate main "public/index.html" [message]
     [:#posts]
-             (append (html-snippet (html message))))
+             (html-content (html message)))
 
 (defpage "/" []
   (content-type "text/html"
     (main (html (map (fn [p] [:div {:class "post"}
                               [:h1 (p :title)]
-                              [:p {class "date"} (p :date)]
-                              [:p {class "post-body"} (p :body)]])
+                              [:p {:class "date"} (p :date)]
+                              [:p {:class "post-body"} (p :body)]])
                      (posts/find-most-recent-five))))))
