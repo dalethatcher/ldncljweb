4fdb224175f1f0b174074e9af73b86bd9d92a621
Rami Chowdhury <rami.chowdhury@gmail.com>
2012-06-16 20:16:28 +0100

    Add compiled ClojureScript to common layout

--- src/ldncljweb/views/common.clj
+++ src/ldncljweb/views/common.clj
@@ -1,12 +1,15 @@
 (ns ldncljweb.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page-helpers :only [include-css html5]]))
+        [hiccup.page-helpers :only [include-css include-js html5]]))
 
 (defpartial layout [& content]
             (html5
               [:head
                [:title "ldncljweb"]
-               (include-css "/css/reset.css")]
+               (include-css "/css/reset.css")
+               (include-js "//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js")
+               (include-js "/js/cljs.js")
+               ]
               [:body
                [:div#wrapper
                 content]]))
