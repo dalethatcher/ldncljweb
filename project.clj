9abb3471c288b02b1d1cd943b010512e8727dee5
Rami Chowdhury <rami.chowdhury@gmail.com>
2012-06-16 20:14:41 +0100

    Add jayq jQuery integration

--- project.clj
+++ project.clj
@@ -4,12 +4,17 @@
                            [noir "1.2.1"]
                            [enlive "1.0.0"]
                            [congomongo "0.1.9"]
-                           [clj-time "0.4.2"]]
+                           [clj-time "0.4.2"]
+                           ;; ClojureScript
+                           [jayq "0.1.0-alpha2"]
+                           ;; [fetch "0.1.0-alpha2"]
+                           ]
             :dev-dependencies [[lein-cljsbuild "0.0.13"]] ; cljsbuild plugin
             :cljsbuild {:source-path "src-cljs"
                         :compiler {:output-to "resources/public/js/cljs.js"
                                    :optimizations :simple
                                    :pretty-print true
+                                   ;; :externs ["externs/jquery.js"]
                                    }
                         }
             :main ldncljweb.server)
