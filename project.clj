0fb7a391cfb95742030cce7d8f78b8f30f0fa778
Stuart Caborn <stooferus@gmail.com>
2012-06-16 14:35:31 +0100

    verify browser assertion with browserid.org

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
                            [noir "1.2.1"]
                            [enlive "1.0.0"]
                            [congomongo "0.1.9"]
-                           [clj-time "0.4.2"]]
+                           [clj-time "0.4.2"]
+                           [clj-http "0.4.3"]]
             :main ldncljweb.server)
-
--- src/ldncljweb/views/login.clj
+++ src/ldncljweb/views/login.clj
@@ -1,11 +1,20 @@
 (ns ldncljweb.views.login
   (:use [noir.core :only [defpage]]
         [noir.response :only [json]])
-  (:require [ldncljweb.models.posts :as posts]))
+  (:require [ldncljweb.models.posts :as posts] [clj-http.client :as client]))
+
+(defn verify-browserid-assertion [assertion]
+  (:body
+   (client/post "https://browserid.org/verify"
+                {:form-params
+                 {:assertion assertion
+                  :audience "http://localhost:8080"}
+                 :throw-exceptions false})))
 
 (defpage "/admin/posts/:id" {id :id}
   (json (posts/find-post id)))
 
 (defpage [:post "/login"] {assertion :assertion}
-  (json {:hai assertion
+
+  (json {:hai  (verify-browserid-assertion assertion)
          :user "bob"}))
