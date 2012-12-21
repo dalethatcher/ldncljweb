4be2cfd3921050c4464a4e926e7af1076f562757
Dale Thatcher <github@dalethatcher.com>
2012-12-21 17:39:06 +0100

    Add extraction by content-type.

--- src/ldncljweb/tools/add_announcement.clj
+++ src/ldncljweb/tools/add_announcement.clj
@@ -24,3 +24,30 @@
   (let [[raw-header body] (cs/split message #"\n\n" 2)]
     [(parse-header raw-header) body]))
 
+(defn seq-matches? [a b]
+  (empty? (drop-while true? (map = a b))))
+
+(defn split-at-boundry 
+  ([boundry body] (split-at-boundry (seq (str "--" boundry)) (seq body) [] [] ))
+  ([boundry remainder result acc]
+    (cond
+      (empty? remainder) result
+      (seq-matches? boundry remainder) (recur boundry
+                                              (drop (count boundry) remainder)
+                                              (conj result (apply str acc))
+                                              [])
+      :else (recur boundry
+                   (rest remainder)
+                   result
+                   (conj acc (first remainder))))))
+  
+(defn content-type [message]
+  (let [[headers _] message
+        raw-content-type (headers :Content-Type)]
+    (first (cs/split raw-content-type #";"))))
+
+(defn parse-body-into-types [boundry body]
+  (let [parts (filter #(not (empty? %)) (split-at-boundry boundry body))
+        parsed-parts (map parse-message parts)]
+    (reduce #(conj %1 [(content-type %2) (second %2)]) {} parsed-parts)
+    ))
--- test/ldncljweb/tools/add_announcement_test.clj
+++ test/ldncljweb/tools/add_announcement_test.clj
@@ -1,15 +1,31 @@
 (ns ldncljweb.tools.add-announcement-test
-  (:use [clojure.test]
-        [ldncljweb.tools.add-announcement]))
+  (:use [clojure.test])
+  (:require [ldncljweb.tools.add-announcement :as aa]))
 
 (def example (slurp "doc/announcement"))
 
-(def simple-example (str "header-1: value-1\n"
+(def simple-message (str "header-1: value-1\n"
                          "header-2: value-2\n"
                          "\n"
                          "This is the body\nof the message"))
 
 (deftest can-read-parse-header-and-body
-  (let [[head body] (parse-message simple-example)]
+  (let [[head body] (aa/parse-message simple-message)]
     (is (= head {:header-1 "value-1" :header-2 "value-2"}))
     (is (= body "This is the body\nof the message"))))
+
+(def multipart-body (str "--boundry"
+                         "Content-Type: text/plain; charset=ISO-8859-1\n"
+                         "\n"
+                         "Some text\n"
+                         "--boundry"
+                         "Content-Type: text/html; charset=ISO-8859-1\n"
+                         "\n"
+                         "<b>Some text</b>\n"
+                         "--boundry--"))
+
+(deftest can-split-body-by-boundry
+  (let [parts-by-type (aa/parse-body-into-types "boundry" multipart-body)]
+    (is (= (parts-by-type "text/plain") "Some text\n"))
+    (is (= (parts-by-type "text/html") "<b>Some text</b>\n"))))
+  
